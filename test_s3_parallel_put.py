66392dc6aec30ee2f5be2d91ffccfcae88565d9e
crccheck <c@crccheck.com>
2014-11-17 18:08:28 -0600

    Test some more combinations of gzip options

--- test_s3_parallel_put.py
+++ test_s3_parallel_put.py
@@ -39,7 +39,6 @@ class PutterTest(unittest.TestCase):
 
     def mock_put(self, bucket, key_name, value):
         """Helper to test that `putter` puts things the way we expect."""
-        print bucket, key_name  # DELETEME
         self.last_key_put = mock.MagicMock()
         return self.last_key_put
 
@@ -56,13 +55,67 @@ class PutterTest(unittest.TestCase):
         headers = args[1]
         self.assertNotIn('Content-Encoding', headers)
 
-    def test_gzip_option_can_gzip_everything(self):
-        # --gzip --gzip-type=all
+    def test_gzip_option_with_zippable_content_means_gzip_content(self):
+        """--gzip"""
         options = mock.MagicMock(
             dry_run=False,  # to get mocked 'put' to do work
-            content_type='binary/thingy',  # a content type not normally gzipped
-            gzip=True,  # enable gzip support
-            gzip_type=['all'],  # force gzipping all
+            content_type='text/html',  # a compressable type
+            gzip=True,
+            gzip_type=[],
+        )
+        # sanity check
+        self.assertIn(options.content_type, s3_parallel_put.GZIP_CONTENT_TYPES)
+        s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
+        args, kwargs = self.last_key_put.set_contents_from_string.call_args
+        # assert that gzip changed the content
+        self.assertNotEqual(args[0], 'boo')
+        headers = args[1]
+        self.assertEqual(headers['Content-Type'], options.content_type)
+        self.assertEqual(headers['Content-Encoding'], 'gzip')
+
+    def test_gzip_option_and_guessed_type_can_gzip(self):
+        """--gzip --gzip-type=guess"""
+        options = mock.MagicMock(
+            dry_run=False,  # to get mocked 'put' to do work
+            content_type='text/html',  # a compressable type
+            gzip=True,
+            gzip_type=['guess'],
+        )
+        # sanity check
+        self.assertIn(options.content_type, s3_parallel_put.GZIP_CONTENT_TYPES)
+        s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
+        args, kwargs = self.last_key_put.set_contents_from_string.call_args
+        # assert that gzip changed the content
+        self.assertNotEqual(args[0], 'boo')
+        headers = args[1]
+        self.assertEqual(headers['Content-Type'], options.content_type)
+        self.assertEqual(headers['Content-Encoding'], 'gzip')
+
+    def test_gzip_option_and_guessed_type_skips_unknown(self):
+        """--gzip --gzip-type=guess"""
+        options = mock.MagicMock(
+            dry_run=False,  # to get mocked 'put' to do work
+            content_type='foo/bar',  # a random type
+            gzip=True,
+            gzip_type=['guess'],
+        )
+        # sanity check
+        self.assertNotIn(options.content_type, s3_parallel_put.GZIP_CONTENT_TYPES)
+        s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
+        args, kwargs = self.last_key_put.set_contents_from_string.call_args
+        # assert that the content did not change
+        self.assertEqual(args[0], 'boo')
+        headers = args[1]
+        self.assertEqual(headers['Content-Type'], options.content_type)
+        self.assertNotIn('Content-Encoding', headers)
+
+    def test_gzip_option_and_guessed_type_and_explicit_type_finds_usual(self):
+        """--gzip --gzip-type=guess --gzip-type="foo/bar" """
+        options = mock.MagicMock(
+            dry_run=False,  # to get mocked 'put' to do work
+            content_type='text/html',  # a compressable type
+            gzip=True,
+            gzip_type=['guess', 'foo/bar'],
         )
         s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
         args, kwargs = self.last_key_put.set_contents_from_string.call_args
@@ -72,18 +125,33 @@ class PutterTest(unittest.TestCase):
         self.assertEqual(headers['Content-Type'], options.content_type)
         self.assertEqual(headers['Content-Encoding'], 'gzip')
 
-    def test_gzip_option_with_zippable_content_means_gzip_content(self):
+    def test_gzip_option_and_guessed_type_and_explicit_type_finds_explicit(self):
+        """--gzip --gzip-type=guess --gzip-type="foo/bar" """
         options = mock.MagicMock(
-            dry_run=False,
-            content_type='text/html',
+            dry_run=False,  # to get mocked 'put' to do work
+            content_type='foo/bar',  # we're saying this is compressable
             gzip=True,
-            gzip_type=['guess'],
+            gzip_type=['guess', 'foo/bar'],
         )
-        # sanity check
-        self.assertIn(options.content_type, s3_parallel_put.GZIP_CONTENT_TYPES)
         s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
         args, kwargs = self.last_key_put.set_contents_from_string.call_args
-        # assert that the content changed
+        # assert that gzip changed the content
+        self.assertNotEqual(args[0], 'boo')
+        headers = args[1]
+        self.assertEqual(headers['Content-Type'], options.content_type)
+        self.assertEqual(headers['Content-Encoding'], 'gzip')
+
+    def test_gzip_option_can_gzip_everything(self):
+        """--gzip --gzip-type=all"""
+        options = mock.MagicMock(
+            dry_run=False,  # to get mocked 'put' to do work
+            content_type='binary/thingy',  # a content type not normally gzipped
+            gzip=True,  # enable gzip support
+            gzip_type=['all'],  # force gzipping all
+        )
+        s3_parallel_put.putter(self.mock_put, self.put_queue, self.stat_queue, options)
+        args, kwargs = self.last_key_put.set_contents_from_string.call_args
+        # assert that gzip changed the content
         self.assertNotEqual(args[0], 'boo')
         headers = args[1]
         self.assertEqual(headers['Content-Type'], options.content_type)
